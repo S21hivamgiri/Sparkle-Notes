d77b24310a2614e308d8904d00fbdbb8e1fd3d84
S21hivamgiri <shivamgiri2015@gmail.com>
2021-12-22 14:38:42 +0530

    Added icon and made navbar.

--- sparkle-notes/src/app/app.module.ts
+++ sparkle-notes/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
